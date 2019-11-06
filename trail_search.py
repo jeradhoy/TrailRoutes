86da8600cbd16c6ee9cae08b20382fca0b6bbafc
Jerad Hoy <jeradhoy@gmail.com>
2019-11-06 15:31:15 -0700

    Got loops and paths working

--- trail_search.py
+++ trail_search.py
@@ -3,39 +3,132 @@
 
 from typing import *
 
-#[ startNode, endNode, dist]
-edge_list = [
-    [0, 1, 1.5],
-    [1, 2, 2],
-    [1, 2, 3],
-    [2, 3, 2.5]
+# trail_num, junct1, Node2, dist
+
+trail_list = [
+    ["1", "a", "b", 1],
+    ["2", "b", "c", 1.5],
+    ["3", "b", "c", 2],
+    ["4", "c", "d", 2]
 ]
-edge_dict = {
-    0: [(1, 1.5)],
-    1: [(2, 2), (2, 3), (0, 1.5)],
-    2: [(3, 2.5), (1, 2), (1, 3)]
+
+def create_trail_dict(trail_list: List) -> Dict:
+    trail_dict = {}
+    for trail in trail_list:
+        trail_dict[trail[0]] = {"junct1": trail[1], "junct2": trail[2], "dist": trail[3]}
+    return trail_dict
+
+def create_node_dict(trail_list: List) -> Dict:
+    node_dict = {}
+
+    for trail in trail_list:
+        start_node = trail[1]
+        if start_node not in node_dict:
+            node_dict[start_node] = []
+        node_dict[start_node].append({"trail": trail[0], "end": trail[2], "dist": trail[3]})
+
+    for trail in trail_list:
+        start_node = trail[2]
+        if start_node not in node_dict:
+            node_dict[start_node] = []
+        node_dict[start_node].append({"trail": trail[0], "end": trail[1], "dist": trail[3]})
+    return node_dict
+
+def create_node_dict(trail_list: List) -> Dict:
+    node_dict = {}
+
+    for trail in trail_list:
+        start_node = trail[1]
+        if start_node not in node_dict:
+            node_dict[start_node] = []
+        node_dict[start_node].append((trail[2], trail[0], trail[3]))
+
+    for trail in trail_list:
+        start_node = trail[2]
+        if start_node not in node_dict:
+            node_dict[start_node] = []
+        node_dict[start_node].append((trail[1], trail[0], trail[3]))
+
+    return node_dict
+
+
+
+def create_node_dict2(trail_list: List) -> Dict:
+    node_dict = {}
+
+    for trail in trail_list:
+        start_node = trail[1]
+        if start_node not in node_dict:
+            node_dict[start_node] = []
+        node_dict[start_node].append((trail[2], trail[3]))
+
+    for trail in trail_list:
+        start_node = trail[2]
+        if start_node not in node_dict:
+            node_dict[start_node] = []
+        node_dict[start_node].append((trail[1], trail[3]))
+    return node_dict
+
+
+trail_dict = create_trail_dict(trail_list)
+node_dict = create_node_dict(trail_list)
+node_dict2 = create_node_dict2(trail_list)
+
+my_graph = {
+    "a": [("b", "1", 1)],
+    "b": [("c", "2", 1.5), ("c", "3", 2)],
+    "c": [("d", "4", 3)],
+    "d": [("e", "5", 2), ("f", "6", 3)],
+    "f": [("g", "7", 3)]
+
 }
 
-paths = []
+def find_all_paths(graph, current_node, max_dist, target_node=None, trail_to=None, current_dist=0, path=[]):
 
-depth_limit = 10
 
-#edge_dict: Dict[List[Tuple]], 
-def find_loops(path: list, current_edge: Tuple, start_node: int, dist, depth):
+    path = path + [(current_node, trail_to, current_dist)]
+    paths = []
 
-    dist += current_edge[1]
-    path += current_edge
-    depth += 1
+    # Only append each path if looking for loops...
+    if target_node is None:
+        paths.append(path)
 
-    # Base case
-    if current_edge[0] == start_node or depth == depth_limit:
-        return (path, dist)
+    if current_dist > max_dist or current_node not in graph or current_node == target_node:
+        return [path]
 
-    # Recursive case
-    next_edge = edge_dict[current_edge[0]]
 
-    for edge in next_edge:
-        find_loops(path, edge, start_node, dist)
+    for next_node in graph[current_node]:
 
-start_node_id = 0
-find_loops([], (0, 1), 0, 0)
+        if next_node[0] not in [node[0] for node in path]:
+
+            new_dist = current_dist + next_node[2]
+
+            newpaths = find_all_paths(graph, next_node[0], max_dist, target_node, next_node[1], new_dist, path)
+
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+
+# So this finds dfs paths away from 
+
+def find_butted_paths(path_list, min_dist, max_dist):
+    path_ends = [(path[-1][0], path[-1][2]) for path in path_list]
+    loop_list = []
+    for i, x in enumerate(path_ends):
+        for j, y in enumerate(path_ends):
+            if x[0] == y[0]:
+                total_dist = x[1] + y[1]
+                if total_dist < max_dist and total_dist > min_dist and (j, i, total_dist) not in loop_list:
+                    loop_list.append((i, j, total_dist))
+    return loop_list
+
+
+
+def find_loops(graph, start_node, min_dist, max_dist):
+
+    all_paths = find_all_paths(graph, start_node, max_dist/2)
+    butts = find_butted_paths(all_paths, min_dist, max_dist)
+    return [([path[1] for path in all_paths[butt[0]] if path[1] is not None] + [path[1] for path in all_paths[butt[1]][::-1] if path[1] is not None], butt[2]) for butt in butts]
+
+find_loops(node_dict, "a", 5, 10)
