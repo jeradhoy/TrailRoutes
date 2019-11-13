d78cc9400437bf9e61e6dc347a9ed5931b08723d
Jerad Hoy <jeradhoy@gmail.com>
2019-11-13 10:14:52 -0700

    meow

--- download.py
+++ download.py
@@ -15,15 +15,28 @@ state_links = [link for link in links if ".zip" in link and "NATIONAL" not in li
 for link in state_links:
     urllib.request.urlretrieve(link, "./Data/" + link.split("/")[-1])
 
+CONFIG = {}
+CONFIG["DataDir"] = "./Data/"
+
+files = os.listdir(CONFIG["DataDir"])
+zip_files = [CONFIG["DataDir"] + filename for filename in files if ".zip" in filename]
+zip_files
 
 
+
+import os
 from zipfile import ZipFile
-with ZipFile("./Data/TRAN_Montana_State_Shape.zip") as zipObj:
-    file_list = zipObj.namelist()
-    trail_files = [filename for filename in file_list if "Trans_Trail" in filename]
-    for trail_file in trail_files:
-        zipObj.extract(trail_file, "./Data/Shapefiles/")
+
+def unzip_files_that_match(zip_path: str, unzip_dir: str, match_str: str) -> str:
+    with ZipFile(zip_path) as zipObj:
+        file_list = zipObj.namelist()
+        trail_files = [filename for filename in file_list if match_str in filename]
+        unzip_path_suffix = zip_path.split("/")[-1].split(".")[0]
+        for trail_file in trail_files:
+            zipObj.extract(trail_file, unzip_dir + unzip_path_suffix)
 
 
+for zip_file in zip_files:
+    unzip_files_that_match(zip_file, CONFIG["DataDir"] + "Shapefiles/", "Trans_Trail")
 
 
