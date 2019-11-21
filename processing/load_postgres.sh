057db20032a356e5c2f06c93769e208f1a686b0d
Jerad Hoy <jeradhoy@gmail.com>
2019-11-21 15:52:13 -0700

    Added setup comments

--- processing/load_postgres.sh
+++ processing/load_postgres.sh
@@ -1,10 +1,15 @@
+# Install postgres:
+# sudo apt install postgresql postgresql-contrib
+# Install postgis
+# sudo apt install postgis postgresql-11-postgis-2.5
 sudo -u postgres createdb trailDb
 sudo -u postgres psql -d trailDb -c "CREATE EXTENSION postgis;"
-sudo -u postgres psql -d trailDb -c "CREATE EXTENSION pgrouting;"
+#sudo -u postgres psql -d trailDb -c "CREATE EXTENSION pgrouting;"
 
 sudo -u postgres psql -d trailDb -c "DROP TABLE IF EXISTS trails;"
 sudo -u postgres psql -d trailDb -c "DROP TABLE IF EXISTS endpoints;"
 
 sudo su postgres
-shp2pgsql -d /home/meow/Classes/Databases/Polyglot/Data/Trail_Cleaned trails | psql -U postgres -d trailDb
-shp2pgsql -d /home/meow/Classes/Databases/Polyglot/Data/Endpoints_Cleaned endpoints | psql -U postgres -d trailDb
+# Path needs to be basically to the name of the shapefile, not the folder it is in
+shp2pgsql -d /home/dad/Classes/Databases/Trail_Routes/Data/trail_processed trails | psql -U postgres -d trailDb
+shp2pgsql -d /home/dad/Classes/Databases/Trail_Routes/Data/junctions_processed endpoints | psql -U postgres -d trailDb
