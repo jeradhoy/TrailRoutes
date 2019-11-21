5fd6e314700c9e15069c6ba0c8a4ef7c8600c803
Jerad Hoy <jeradhoy@gmail.com>
2019-11-21 15:52:43 -0700

    Removed old code, renamed main table

--- processing/process.sql
+++ processing/process.sql
@@ -1,5 +1,6 @@
 DROP TABLE junctions;
 DROP TABLE junct_trail;
+DROP TABLE trail_junct_rel;
 
 -- Create unique junctions table
 CREATE TABLE junctions AS
@@ -12,10 +13,9 @@ SELECT id as trail_id, junct_id FROM endpoints
 LEFT JOIN junctions
 ON ST_X(endpoints.geom) = ST_X(junctions.geom) and ST_Y(endpoints.geom) = ST_Y(junctions.geom);
 
-DROP TABLE junct_rel;
 
-CREATE TABLE junct_rel AS
-
+-- Combine trails and junctions to get to-from points in trail dataset
+CREATE TABLE trail_junct_rel AS
 WITH junct_split as (
     SELECT trail_id,
     min(junct_id) AS junct1,
@@ -27,62 +27,8 @@ SELECT name, trailnumbe, id::INTEGER, length_mi, junct1, junct2, geom FROM trail
 LEFT JOIN junct_split ON junct_split.trail_id = trails.id;
 
 
-select id from junct_rel limit 5;
-6228
-6230
-SELECT id, junct1 as source, junct2 as target, length_mi as cost from junct_rel;
-
-WITH ksp AS (
-    SELECT  * FROM  pgr_KSP(
-            'SELECT id, junct1 as source, junct2 as target, length_mi as cost from junct_rel',
-            6228, 6230, 3, 
-            directed := FALSE
-        )
-)
-SELECT * FROM trails 
-RIGHT JOIN ksp.edge = trails.trail_id WHERE ksp.path_id = 1;
-
-SELECT id from junct_rel;
-
-SELECT * FROM  pgr_KSP(
-        'SELECT id, junct1 as source, junct2 as target, length_mi as cost from junct_rel',
-        5488, 5487, 10, 
-        directed := FALSE
-    )
-
-WITH ksp AS (
-    SELECT  seq, edge FROM  pgr_KSP(
-            'SELECT id, junct1 as source, junct2 as target, length_mi as cost from junct_rel',
-            5488, 5487, 10, 
-            directed := FALSE
-        )
-    WHERE path_id = 1
-) SELECT * FROM junct_rel RIGHT JOIN ksp ON ksp.edge = junct_rel.id;
-
-
-
-
-
-
-
-
-
-
-
 -- Dump trail relationships to CSV
-COPY (
-    WITH junct_split as (
-        SELECT trail_id,
-        min(junct_id) AS junct1,
-        max(junct_id) AS junct2
-        FROM junct_trail
-        GROUP BY trail_id
-    )
-    SELECT name, trailnumbe, id, length_mi, junct1, junct2 FROM trails
-    LEFT JOIN junct_split ON junct_split.trail_id = trails.id
-) TO '/tmp/trailRelationship.csv' WITH CSV header;
+COPY (SELECT id, length_mi, junct1, junct2 FROM trail_junct_rel) TO '/tmp/trail_relationship.csv' WITH CSV header;
 
 -- Dump trail junctions to csv
 COPY (SELECT junct_id FROM junctions) TO '/tmp/junctions.csv' WITH CSV header;
-
--- Need to move to Neo4j folder
