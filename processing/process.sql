7a0503c5bf9e03f2957f0bb7f81d1fddf95f68bc
jeradhoy <jeradhoy@gmail.com>
2019-12-10 02:47:26 +0000

    Optimization steps to make db faster

--- processing/process.sql
+++ processing/process.sql
@@ -7,13 +7,11 @@ CREATE TABLE junctions AS
 SELECT DISTINCT ON (ST_X(geom), ST_Y(geom)) geom FROM endpoints;
 ALTER TABLE junctions ADD COLUMN junct_id SERIAL PRIMARY KEY;
 
-
 -- Create join table for junctions and trails
 -- CREATE TABLE junct_trail AS
 -- SELECT id as trail_id, junct_id FROM endpoints
 -- LEFT JOIN junctions
 -- ON ST_X(endpoints.geom) = ST_X(junctions.geom) and ST_Y(endpoints.geom) = ST_Y(junctions.geom);
-
 
 -- Combine trails and junctions to get to-from points in trail dataset
 CREATE TABLE trail_junct_rel AS
@@ -32,9 +30,20 @@ junct_split as (
 SELECT name, trailnumbe, id::INTEGER, length_mi, junct1, junct2, geom FROM trails
 LEFT JOIN junct_split ON junct_split.trail_id = trails.id;
 
+UPDATE trail_junct_rel SET geom = ST_Transform(ST_SetSRID(geom, 4326), 3857);
+UPDATE junctions SET geom = ST_Transform(ST_SetSRID(geom, 4326), 3857);
 
 DROP TABLE endpoints;
 DROP TABLE trails;
+
+DROP INDEX trails_geom_gist;
+CREATE INDEX trails_geom_gist ON trail_junct_rel USING GIST (geom);
+CLUSTER  trail_junct_rel USING trails_geom_gist;
+
+DROP INDEX junctions_geom_gist;
+CREATE INDEX junctions_geom_gist ON junctions USING GIST (geom);
+CLUSTER junctions USING junctions_geom_gist;
+-- SET work_mem TO '256MB';
 
 -- Only relevant tables are 'trail_junct_rel' and 'junctions'
 
